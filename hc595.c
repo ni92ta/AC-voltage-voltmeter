38c3b261a80998c3858ea0416c5ab89e617b8c79
Nikita Romanovich <91215296+ni92ta@users.noreply.github.com>
2022-06-09 19:39:36 +0300

    Update hc595.c

--- hc595.c
+++ hc595.c
@@ -10,46 +10,43 @@
 #include <xc.h>
 unsigned char i;
 
-//unsigned char digit [11] = { 0b00111111, 0b00000110, 0b01011011, 0b01001111, 0b01100110,
-//0b01101101, 0b01111101, 0b00000111, 0b01111111, 0b01101111, 0b00000000 
-    
-//};
-
 //-------------------отправка данных--------------------------------------------
 void senddata (unsigned char data, unsigned char data2, unsigned char data3){
     unsigned char outt;
     unsigned char u = 0;
+      for (cirkle=0;cirkle<2;cirkle++){//цикл добавлен так как не показывало одну цифру
+//нужно разобраться почему
     for (u=0;u<3;u++){   
 RCLK = 0;            
 for (i=0;i<8;i++){   
     SRCLK = 0;
     __delay_us(1);
-    if (outt & 0x80){
-        SER = 1;
+    if (outt & 0x80){//сравниваем значение с битовой маской
+        SER = 1;//записываем бит в регистр
     }
     else{
-        SER = 0;
+        SER = 0;//записываем бит в регистр
     }
-    outt=(outt<<1);
+    outt=(outt<<1);//сдвигаем байт на 1
     SRCLK = 1;
     __delay_us(1);
                 }  
  RCLK = 1; 
  DIG1 = 1; 
 __delay_ms(5);
-                    if (u == 2){
+                    if (u == 1){
                     outt = data;
                                }
             
-                    if (u == 0){
+                    if (u == 2){
                     outt = data2;
                                }
 
-                    if (u == 1){
+                    if (u == 0){
                     outt = data3;
                                }
 DIG1 = 0;
-
+ }
                    }    
 }
 
